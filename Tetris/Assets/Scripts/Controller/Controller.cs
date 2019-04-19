823dc9f37940f2e64100c836d328fd5a2f903614
QingRang <970753519@qq.com>
2019-04-19 12:48:28 +0800

    View 层的搭建
    主要是游戏的一些界面

---
+++ Tetris/Assets/Scripts/Controller/Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 控制器
+/// </summary>
+public class Controller : MonoBehaviour {
+
+    // 获取 Model 和 View 的引用
+    [HideInInspector]
+    public Model model;
+    public View view;
+
+    private void Awake()
+    {
+        // 通过标签查找，获取相应的组件
+        model = GameObject.FindGameObjectWithTag("Model").GetComponent<Model>();
+        view = GameObject.FindGameObjectWithTag("View").GetComponent<View>();
+    }
+}
